9308cbdb7e9037e423f0f253f8624a8a0736c7f7
Prashastini-n <prashastinin@gmail.com>
2026-04-18 19:41:47 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -22,7 +22,7 @@ st.markdown("---")
 # LOAD DATA
 # ---------------------------------------------------
 
-df = pd.read_csv(r"C:\Users\Prashastini N\Desktop\nassau-candy-profitability-analysis\data\cleaned_data.csv")
+df = pd.read_csv(r"data\cleaned_data.csv")
 
 # Create calculated metrics
 df["Margin"] = (df["Gross Profit"] / df["Sales"]) * 100
